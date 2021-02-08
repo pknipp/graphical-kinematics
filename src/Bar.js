6c8bd0cd393ddb3243570216f0a0995f47dede9c
Peter Knipp <peteraknipp@gmail.com>
2021-02-08 08:56:49 -0500

    .env

--- src/Bar.js
+++ src/Bar.js
@@ -1,8 +1,11 @@
 import React from "react";
 const Bar = ({ j, dt, y, y1, color }) => {
+    // Following two are sidelengths of small right triangle whose hypotenuse is the graph segment.
     let t = j * dt;
     let dy = y1 - y;
+    // Use pythagorean theorem to translate from cartesian to geometric representation.
     let r = Math.sqrt(dt * dt + dy * dy);
+    // Use arctangent to complete the cartesian/geometric transformation.
     let angle = Math.atan2(dy, dt) * 180 / Math.PI;
     return (
         <div className="segment"
